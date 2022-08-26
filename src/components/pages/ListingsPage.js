ceb1456ddf376169e521bfcdef17833307bbdd82
theonlyouan <juanjovanegas21@gmail.com>
2022-08-26 15:28:14 -0400

    Refactor header to own component

---
+++ src/components/pages/ListingsPage.js
@@ -0,0 +1,13 @@
+const ListingsPage = () => {
+  return (
+    <article>
+      <section>
+        <p>Address</p>
+        <p>Price</p>
+        <p>Availability</p>
+      </section>
+    </article>
+  );
+};
+
+export default ListingsPage;
