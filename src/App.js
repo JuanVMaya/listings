a24a33958cbc3ab72e129de8a187f0efd7c078eb
theonlyouan <juanjovanegas21@gmail.com>
2022-08-26 14:53:18 -0400

    Create header and footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.scss';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <p>Start app</p>
-    </div>
+    <BrowserRouter>
+      <header className="header">
+        <h1 className="header__logo">LISTR</h1>
+        <button className="button">+ Add Listing</button>
+      </header>
+      <div className="main-container">
+        <Switch>
+          {/* <Route path="/" exact component={ListingsPage} /> */}
+        </Switch>
+      </div>
+      <footer className="footer">
+        <h2>Coded by Juan J Vanegas Maya - 2022</h2>
+      </footer>
+    </BrowserRouter>
   );
 }
 
