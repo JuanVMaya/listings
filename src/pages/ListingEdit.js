b2d9390b9b9f2beabb78e2c2684a53d20dd26f71
theonlyouan <juanjovanegas21@gmail.com>
2022-08-27 10:47:12 -0400

    Create form onSubmit handler. API call to update listing endpoint

--- src/pages/ListingEdit.js
+++ src/pages/ListingEdit.js
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useState, useEffect, useCallback } from "react";
+import { useParams, Link, useHistory } from "react-router-dom";
 import axios from "axios";
 import "./ListingEdit.scss";
 
@@ -10,21 +10,23 @@ const ListingEdit = () => {
   const [postalCode, setPostalCode] = useState("");
   const [price, setPrice] = useState("");
   const [available, setAvailable] = useState(null);
+  const [updatedConfirmation, setUpdatedConfirmation] = useState(false);
+
+  const history = useHistory();
   const { id } = useParams();
 
-  const getListing = async () => {
+  const getListing = useCallback(async () => {
     const res = await axios.get(API_URL + id);
     const { data } = res;
-    console.log(data);
     setAddress(data.address);
     setPostalCode(data.postalCode);
     setPrice(data.price);
     setAvailable(data.available);
-  };
+  }, [id]);
 
   useEffect(() => {
     getListing();
-  }, []);
+  }, [getListing]);
 
   const handleAddressChange = (event) => {
     // Validation to be added here
@@ -41,11 +43,27 @@ const ListingEdit = () => {
   const handleAvailableChange = (event) => {
     setAvailable(event.target.value === "true"); // Use the expression to turn the value string given by the target value into a boolean
   };
+  const handleEditFormSubmit = async (event) => {
+    event.preventDefault();
+    const submittedData = {
+      address: address,
+      available: available,
+      price: price,
+      postalCode: postalCode,
+    };
+    const res = await axios.put(API_URL + id, submittedData);
+    console.log(res.data);
+    // getListing();
+    setUpdatedConfirmation(true);
+    setTimeout(() => {
+      history.push("/listings");
+    }, 3000);
+  };
 
   return (
     <section>
       <h1>Edit Listing</h1>
-      <form className="edit-form">
+      <form className="edit-form" onSubmit={handleEditFormSubmit}>
         <div className="input-actions">
           <label className="input-actions__label">
             Address
@@ -109,6 +127,9 @@ const ListingEdit = () => {
           <Link type="button" to="/">
             <button className="button">Cancel</button>
           </Link>
+          {updatedConfirmation && (
+            <p>The listing has been updated. Redirecting to home...</p>
+          )}
         </div>
       </form>
     </section>
